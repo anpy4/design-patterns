d5b9fab6de5a28e1c46ee6f9df51fe6c85e3e2f5
anpy4 <cgndre@yahoo.com>
2022-09-26 16:12:04 +0200

    Add base structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='patterns',
+   version='1.0.0',
+   description='This module implements design patterns frequently used in object-oriented programming',
+   author='André Schiefer',
+   author_email='cgndre@yahoo.com',
+   packages=['src/patterns'],
+   tests_requires=['pytest']
+)
---
+++ tests/test_sum.py
@@ -0,0 +1,4 @@
+from patterns import sum
+
+def test_sum():
+    assert sum.sum(3,4) == 7
